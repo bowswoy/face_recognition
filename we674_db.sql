b4ced2af440f33db6e94184975526fa351fe2ef4
puwanai_san <puwanai_san@dsi.go.th>
2022-10-21 21:52:46 +0700

    clean db

--- we674_db.sql
+++ we674_db.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost
  Source Server Type    : MariaDB
- Source Server Version : 100424 (10.4.24-MariaDB)
+ Source Server Version : 100421
  Source Host           : localhost:3306
  Source Schema         : we674_db
 
  Target Server Type    : MariaDB
- Target Server Version : 100424 (10.4.24-MariaDB)
+ Target Server Version : 100421
  File Encoding         : 65001
 
- Date: 21/10/2022 20:32:26
+ Date: 21/10/2022 21:52:23
 */
 
 SET NAMES utf8mb4;
@@ -21,232 +21,22 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for img_dataset
 -- ----------------------------
 DROP TABLE IF EXISTS `img_dataset`;
-CREATE TABLE `img_dataset`  (
+CREATE TABLE `img_dataset` (
   `img_id` int(11) NOT NULL,
-  `img_person` int(11) NULL DEFAULT NULL,
+  `img_person` int(11) DEFAULT NULL,
   PRIMARY KEY (`img_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of img_dataset
--- ----------------------------
-INSERT INTO `img_dataset` VALUES (1, 1000001);
-INSERT INTO `img_dataset` VALUES (2, 1000001);
-INSERT INTO `img_dataset` VALUES (3, 1000001);
-INSERT INTO `img_dataset` VALUES (4, 1000001);
-INSERT INTO `img_dataset` VALUES (5, 1000001);
-INSERT INTO `img_dataset` VALUES (6, 1000001);
-INSERT INTO `img_dataset` VALUES (7, 1000001);
-INSERT INTO `img_dataset` VALUES (8, 1000001);
-INSERT INTO `img_dataset` VALUES (9, 1000001);
-INSERT INTO `img_dataset` VALUES (10, 1000001);
-INSERT INTO `img_dataset` VALUES (11, 1000001);
-INSERT INTO `img_dataset` VALUES (12, 1000001);
-INSERT INTO `img_dataset` VALUES (13, 1000001);
-INSERT INTO `img_dataset` VALUES (14, 1000001);
-INSERT INTO `img_dataset` VALUES (15, 1000001);
-INSERT INTO `img_dataset` VALUES (16, 1000001);
-INSERT INTO `img_dataset` VALUES (17, 1000001);
-INSERT INTO `img_dataset` VALUES (18, 1000001);
-INSERT INTO `img_dataset` VALUES (19, 1000001);
-INSERT INTO `img_dataset` VALUES (20, 1000001);
-INSERT INTO `img_dataset` VALUES (21, 1000001);
-INSERT INTO `img_dataset` VALUES (22, 1000001);
-INSERT INTO `img_dataset` VALUES (23, 1000001);
-INSERT INTO `img_dataset` VALUES (24, 1000001);
-INSERT INTO `img_dataset` VALUES (25, 1000001);
-INSERT INTO `img_dataset` VALUES (26, 1000001);
-INSERT INTO `img_dataset` VALUES (27, 1000001);
-INSERT INTO `img_dataset` VALUES (28, 1000001);
-INSERT INTO `img_dataset` VALUES (29, 1000001);
-INSERT INTO `img_dataset` VALUES (30, 1000001);
-INSERT INTO `img_dataset` VALUES (31, 1000001);
-INSERT INTO `img_dataset` VALUES (32, 1000001);
-INSERT INTO `img_dataset` VALUES (33, 1000001);
-INSERT INTO `img_dataset` VALUES (34, 1000001);
-INSERT INTO `img_dataset` VALUES (35, 1000001);
-INSERT INTO `img_dataset` VALUES (36, 1000001);
-INSERT INTO `img_dataset` VALUES (37, 1000001);
-INSERT INTO `img_dataset` VALUES (38, 1000001);
-INSERT INTO `img_dataset` VALUES (39, 1000001);
-INSERT INTO `img_dataset` VALUES (40, 1000001);
-INSERT INTO `img_dataset` VALUES (41, 1000001);
-INSERT INTO `img_dataset` VALUES (42, 1000001);
-INSERT INTO `img_dataset` VALUES (43, 1000001);
-INSERT INTO `img_dataset` VALUES (44, 1000001);
-INSERT INTO `img_dataset` VALUES (45, 1000001);
-INSERT INTO `img_dataset` VALUES (46, 1000001);
-INSERT INTO `img_dataset` VALUES (47, 1000001);
-INSERT INTO `img_dataset` VALUES (48, 1000001);
-INSERT INTO `img_dataset` VALUES (49, 1000001);
-INSERT INTO `img_dataset` VALUES (50, 1000001);
-INSERT INTO `img_dataset` VALUES (51, 1000001);
-INSERT INTO `img_dataset` VALUES (52, 1000001);
-INSERT INTO `img_dataset` VALUES (53, 1000001);
-INSERT INTO `img_dataset` VALUES (54, 1000001);
-INSERT INTO `img_dataset` VALUES (55, 1000001);
-INSERT INTO `img_dataset` VALUES (56, 1000001);
-INSERT INTO `img_dataset` VALUES (57, 1000001);
-INSERT INTO `img_dataset` VALUES (58, 1000001);
-INSERT INTO `img_dataset` VALUES (59, 1000001);
-INSERT INTO `img_dataset` VALUES (60, 1000001);
-INSERT INTO `img_dataset` VALUES (61, 1000001);
-INSERT INTO `img_dataset` VALUES (62, 1000001);
-INSERT INTO `img_dataset` VALUES (63, 1000001);
-INSERT INTO `img_dataset` VALUES (64, 1000001);
-INSERT INTO `img_dataset` VALUES (65, 1000001);
-INSERT INTO `img_dataset` VALUES (66, 1000001);
-INSERT INTO `img_dataset` VALUES (67, 1000001);
-INSERT INTO `img_dataset` VALUES (68, 1000001);
-INSERT INTO `img_dataset` VALUES (69, 1000001);
-INSERT INTO `img_dataset` VALUES (70, 1000001);
-INSERT INTO `img_dataset` VALUES (71, 1000001);
-INSERT INTO `img_dataset` VALUES (72, 1000001);
-INSERT INTO `img_dataset` VALUES (73, 1000001);
-INSERT INTO `img_dataset` VALUES (74, 1000001);
-INSERT INTO `img_dataset` VALUES (75, 1000001);
-INSERT INTO `img_dataset` VALUES (76, 1000001);
-INSERT INTO `img_dataset` VALUES (77, 1000001);
-INSERT INTO `img_dataset` VALUES (78, 1000001);
-INSERT INTO `img_dataset` VALUES (79, 1000001);
-INSERT INTO `img_dataset` VALUES (80, 1000001);
-INSERT INTO `img_dataset` VALUES (81, 1000001);
-INSERT INTO `img_dataset` VALUES (82, 1000001);
-INSERT INTO `img_dataset` VALUES (83, 1000001);
-INSERT INTO `img_dataset` VALUES (84, 1000001);
-INSERT INTO `img_dataset` VALUES (85, 1000001);
-INSERT INTO `img_dataset` VALUES (86, 1000001);
-INSERT INTO `img_dataset` VALUES (87, 1000001);
-INSERT INTO `img_dataset` VALUES (88, 1000001);
-INSERT INTO `img_dataset` VALUES (89, 1000001);
-INSERT INTO `img_dataset` VALUES (90, 1000001);
-INSERT INTO `img_dataset` VALUES (91, 1000001);
-INSERT INTO `img_dataset` VALUES (92, 1000001);
-INSERT INTO `img_dataset` VALUES (93, 1000001);
-INSERT INTO `img_dataset` VALUES (94, 1000001);
-INSERT INTO `img_dataset` VALUES (95, 1000001);
-INSERT INTO `img_dataset` VALUES (96, 1000001);
-INSERT INTO `img_dataset` VALUES (97, 1000001);
-INSERT INTO `img_dataset` VALUES (98, 1000001);
-INSERT INTO `img_dataset` VALUES (99, 1000001);
-INSERT INTO `img_dataset` VALUES (100, 1000001);
-INSERT INTO `img_dataset` VALUES (101, 1000002);
-INSERT INTO `img_dataset` VALUES (102, 1000002);
-INSERT INTO `img_dataset` VALUES (103, 1000002);
-INSERT INTO `img_dataset` VALUES (104, 1000002);
-INSERT INTO `img_dataset` VALUES (105, 1000002);
-INSERT INTO `img_dataset` VALUES (106, 1000002);
-INSERT INTO `img_dataset` VALUES (107, 1000002);
-INSERT INTO `img_dataset` VALUES (108, 1000002);
-INSERT INTO `img_dataset` VALUES (109, 1000002);
-INSERT INTO `img_dataset` VALUES (110, 1000002);
-INSERT INTO `img_dataset` VALUES (111, 1000002);
-INSERT INTO `img_dataset` VALUES (112, 1000002);
-INSERT INTO `img_dataset` VALUES (113, 1000002);
-INSERT INTO `img_dataset` VALUES (114, 1000002);
-INSERT INTO `img_dataset` VALUES (115, 1000002);
-INSERT INTO `img_dataset` VALUES (116, 1000002);
-INSERT INTO `img_dataset` VALUES (117, 1000002);
-INSERT INTO `img_dataset` VALUES (118, 1000002);
-INSERT INTO `img_dataset` VALUES (119, 1000002);
-INSERT INTO `img_dataset` VALUES (120, 1000002);
-INSERT INTO `img_dataset` VALUES (121, 1000002);
-INSERT INTO `img_dataset` VALUES (122, 1000002);
-INSERT INTO `img_dataset` VALUES (123, 1000002);
-INSERT INTO `img_dataset` VALUES (124, 1000002);
-INSERT INTO `img_dataset` VALUES (125, 1000002);
-INSERT INTO `img_dataset` VALUES (126, 1000002);
-INSERT INTO `img_dataset` VALUES (127, 1000002);
-INSERT INTO `img_dataset` VALUES (128, 1000002);
-INSERT INTO `img_dataset` VALUES (129, 1000002);
-INSERT INTO `img_dataset` VALUES (130, 1000002);
-INSERT INTO `img_dataset` VALUES (131, 1000002);
-INSERT INTO `img_dataset` VALUES (132, 1000002);
-INSERT INTO `img_dataset` VALUES (133, 1000002);
-INSERT INTO `img_dataset` VALUES (134, 1000002);
-INSERT INTO `img_dataset` VALUES (135, 1000002);
-INSERT INTO `img_dataset` VALUES (136, 1000002);
-INSERT INTO `img_dataset` VALUES (137, 1000002);
-INSERT INTO `img_dataset` VALUES (138, 1000002);
-INSERT INTO `img_dataset` VALUES (139, 1000002);
-INSERT INTO `img_dataset` VALUES (140, 1000002);
-INSERT INTO `img_dataset` VALUES (141, 1000002);
-INSERT INTO `img_dataset` VALUES (142, 1000002);
-INSERT INTO `img_dataset` VALUES (143, 1000002);
-INSERT INTO `img_dataset` VALUES (144, 1000002);
-INSERT INTO `img_dataset` VALUES (145, 1000002);
-INSERT INTO `img_dataset` VALUES (146, 1000002);
-INSERT INTO `img_dataset` VALUES (147, 1000002);
-INSERT INTO `img_dataset` VALUES (148, 1000002);
-INSERT INTO `img_dataset` VALUES (149, 1000002);
-INSERT INTO `img_dataset` VALUES (150, 1000002);
-INSERT INTO `img_dataset` VALUES (151, 1000002);
-INSERT INTO `img_dataset` VALUES (152, 1000002);
-INSERT INTO `img_dataset` VALUES (153, 1000002);
-INSERT INTO `img_dataset` VALUES (154, 1000002);
-INSERT INTO `img_dataset` VALUES (155, 1000002);
-INSERT INTO `img_dataset` VALUES (156, 1000002);
-INSERT INTO `img_dataset` VALUES (157, 1000002);
-INSERT INTO `img_dataset` VALUES (158, 1000002);
-INSERT INTO `img_dataset` VALUES (159, 1000002);
-INSERT INTO `img_dataset` VALUES (160, 1000002);
-INSERT INTO `img_dataset` VALUES (161, 1000002);
-INSERT INTO `img_dataset` VALUES (162, 1000002);
-INSERT INTO `img_dataset` VALUES (163, 1000002);
-INSERT INTO `img_dataset` VALUES (164, 1000002);
-INSERT INTO `img_dataset` VALUES (165, 1000002);
-INSERT INTO `img_dataset` VALUES (166, 1000002);
-INSERT INTO `img_dataset` VALUES (167, 1000002);
-INSERT INTO `img_dataset` VALUES (168, 1000002);
-INSERT INTO `img_dataset` VALUES (169, 1000002);
-INSERT INTO `img_dataset` VALUES (170, 1000002);
-INSERT INTO `img_dataset` VALUES (171, 1000002);
-INSERT INTO `img_dataset` VALUES (172, 1000002);
-INSERT INTO `img_dataset` VALUES (173, 1000002);
-INSERT INTO `img_dataset` VALUES (174, 1000002);
-INSERT INTO `img_dataset` VALUES (175, 1000002);
-INSERT INTO `img_dataset` VALUES (176, 1000002);
-INSERT INTO `img_dataset` VALUES (177, 1000002);
-INSERT INTO `img_dataset` VALUES (178, 1000002);
-INSERT INTO `img_dataset` VALUES (179, 1000002);
-INSERT INTO `img_dataset` VALUES (180, 1000002);
-INSERT INTO `img_dataset` VALUES (181, 1000002);
-INSERT INTO `img_dataset` VALUES (182, 1000002);
-INSERT INTO `img_dataset` VALUES (183, 1000002);
-INSERT INTO `img_dataset` VALUES (184, 1000002);
-INSERT INTO `img_dataset` VALUES (185, 1000002);
-INSERT INTO `img_dataset` VALUES (186, 1000002);
-INSERT INTO `img_dataset` VALUES (187, 1000002);
-INSERT INTO `img_dataset` VALUES (188, 1000002);
-INSERT INTO `img_dataset` VALUES (189, 1000002);
-INSERT INTO `img_dataset` VALUES (190, 1000002);
-INSERT INTO `img_dataset` VALUES (191, 1000002);
-INSERT INTO `img_dataset` VALUES (192, 1000002);
-INSERT INTO `img_dataset` VALUES (193, 1000002);
-INSERT INTO `img_dataset` VALUES (194, 1000002);
-INSERT INTO `img_dataset` VALUES (195, 1000002);
-INSERT INTO `img_dataset` VALUES (196, 1000002);
-INSERT INTO `img_dataset` VALUES (197, 1000002);
-INSERT INTO `img_dataset` VALUES (198, 1000002);
-INSERT INTO `img_dataset` VALUES (199, 1000002);
-INSERT INTO `img_dataset` VALUES (200, 1000002);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for person_data
 -- ----------------------------
 DROP TABLE IF EXISTS `person_data`;
-CREATE TABLE `person_data`  (
+CREATE TABLE `person_data` (
   `p_id` int(11) NOT NULL,
-  `p_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `p_created` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
-  `p_status` tinyint(4) NULL DEFAULT 1,
+  `p_name` varchar(255) DEFAULT NULL,
+  `p_created` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `p_status` tinyint(4) DEFAULT 1,
   PRIMARY KEY (`p_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of person_data
--- ----------------------------
-INSERT INTO `person_data` VALUES (1000001, 'Namning', '2022-10-21 17:51:13', 1);
-INSERT INTO `person_data` VALUES (1000002, 'PS', '2022-10-21 20:29:48', 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
