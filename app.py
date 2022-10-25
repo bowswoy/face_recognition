287d0cf6db9853be7d53c9e7f66043ddda237f7d
puwanai_san <puwanai_san@dsi.go.th>
2022-10-25 21:15:18 +0700

    add hand check in

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from PIL import Image
 import numpy as np
 import os
 from playsound import playsound
+import mediapipe as mp
+import time
 
 ###################### init flask app ######################
 app = Flask(__name__)
@@ -13,7 +15,7 @@ app = Flask(__name__)
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="",
+    passwd="1234",
     database="we674_db"
 )
 cursor = conn.cursor()
@@ -24,8 +26,11 @@ pause_count = 0
 scanned = False
 
 ###################### function zone ######################
+
+
 def generate_dataset(pid):
-    face_classifier = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
+    face_classifier = cv2.CascadeClassifier(
+        "resources/haarcascade_frontalface_default.xml")
 
     def face_cropped(img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -34,13 +39,15 @@ def generate_dataset(pid):
             return None
         for top, right, bottom, left in faces:
             cropped_face = img[right:right + left, top:top + bottom]
-            cv2.rectangle(img, (top, right), (top + bottom, right + left), (26, 174, 10), 2)
-            cv2.putText(img, "Scanning..", (top + 4, right + left + 14), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (26, 174, 10), 1, cv2.LINE_AA)
+            cv2.rectangle(img, (top, right), (top + bottom,
+                          right + left), (26, 174, 10), 2)
+            cv2.putText(img, "Scanning..", (top + 4, right + left + 14),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (26, 174, 10), 1, cv2.LINE_AA)
         return cropped_face
 
     cap = cv2.VideoCapture(0)
 
-    ## get max id
+    # get max id
     cursor.execute("SELECT IFNULL(MAX(img_id), 0) FROM img_dataset")
     row = cursor.fetchone()
     img_id = row[0]
@@ -63,53 +70,87 @@ def generate_dataset(pid):
                 face_gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
                 file_name_path = "dataset/" + pid + "." + str(img_id) + ".jpg"
                 cv2.imwrite(file_name_path, face_gray)
-                cv2.rectangle(img, (top, right), (top + bottom, right + left), (26, 174, 10), 2)
-                cv2.rectangle(img, (top - 1, right + left), (top + 1 + bottom, right + left + 20), (26, 174, 10), cv2.FILLED)
-                cv2.putText(img, "Scanning.. " + str(count_img) + "%", (top + 4, right + left + 14), cv2.FONT_HERSHEY_DUPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
-                ## insert to db
-                cursor.execute("""INSERT INTO `img_dataset`(`img_id`, `img_person`) VALUES('{}', '{}')""".format(img_id, pid))
+                cv2.rectangle(img, (top, right), (top + bottom,
+                              right + left), (26, 174, 10), 2)
+                cv2.rectangle(img, (top - 1, right + left), (top + 1 +
+                              bottom, right + left + 20), (26, 174, 10), cv2.FILLED)
+                cv2.putText(img, "Scanning.. " + str(count_img) + "%", (top + 4, right +
+                            left + 14), cv2.FONT_HERSHEY_DUPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+                # insert to db
+                cursor.execute(
+                    """INSERT INTO `img_dataset`(`img_id`, `img_person`) VALUES('{}', '{}')""".format(img_id, pid))
                 conn.commit()
                 frame = cv2.imencode('.jpeg', img)[1].tobytes()
                 yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
                 if cv2.waitKey(1) == 13 or int(img_id) == int(max_id):
-                    playsound(r'static\\sounds\\success-sound-effect.mp3', True)
+                    playsound('static/sounds/success-sound-effect.mp3', True)
                     cap.release()
                     cv2.destroyAllWindows()
                     break
 
+
 def get_face_recognition():
-    def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, _, clf):
-        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        features = classifier.detectMultiScale(gray_image, scaleFactor, minNeighbors)
-        for top, right, bottom, left in features:
-            cv2.rectangle(img, (top, right), (top + bottom, right + left), color, 2)
-            cv2.rectangle(img, (top - 1, right + left), (top + 1 + bottom, right + left + 20), color, cv2.FILLED)
-            id, pred = clf.predict(gray_image[right:right + left, top:top + bottom])
-            confidence = int(100 * (1 - pred / 300))
-            cursor.execute("SELECT p.p_name FROM img_dataset AS i LEFT JOIN person_data AS p ON i.img_person = p.p_id WHERE i.img_id = " + str(id))
-            name = cursor.fetchone()
-            if name is not None:
-                name = ''.join(name)
-            if confidence > 70:
-                label = name + " (" + str(confidence) + "%)"
-                cv2.putText(img, label, (top + 4, right + left + 14), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255), 1, cv2.LINE_AA)
-            else:
-                cv2.putText(img, "Unknown", (top + 4, right + left + 14), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255), 1, cv2.LINE_AA)
-        return True
+    # def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, _, clf):
+    #     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #     features = classifier.detectMultiScale(
+    #         gray_image, scaleFactor, minNeighbors)
+    #     for top, right, bottom, left in features:
+    #         cv2.rectangle(img, (top, right),
+    #                       (top + bottom, right + left), color, 2)
+    #         cv2.rectangle(img, (top - 1, right + left), (top +
+    #                       1 + bottom, right + left + 20), color, cv2.FILLED)
+    #         id, pred = clf.predict(
+    #             gray_image[right:right + left, top:top + bottom])
+    #         confidence = int(100 * (1 - pred / 300))
+    #         cursor.execute(
+    #             "SELECT p.p_name FROM img_dataset AS i LEFT JOIN person_data AS p ON i.img_person = p.p_id WHERE i.img_id = " + str(id))
+    #         name = cursor.fetchone()
+    #         if name is not None:
+    #             name = ''.join(name)
+    #         if confidence > 70:
+    #             label = name + " (" + str(confidence) + "%)"
+    #             cv2.putText(img, label, (top + 4, right + left + 14),
+    #                         cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+    #         else:
+    #             cv2.putText(img, "Unknown", (top + 4, right + left + 14),
+    #                         cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+    #     return True
 
-    def recognize(img, clf, faceCascade):
-        draw_boundary(img, faceCascade, 1.1, 10, (26, 174, 10), "Face", clf)
-        return img
+    # def recognize(img, clf, faceCascade):
+    #     draw_boundary(img, faceCascade, 1.1, 10, (26, 174, 10), "Face", clf)
+    #     return img
 
-    faceCascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
+    faceCascade = cv2.CascadeClassifier(
+        "resources/haarcascade_frontalface_default.xml")
     clf = cv2.face.LBPHFaceRecognizer_create()
     clf.read("resources/classifier.xml")
 
     videoCapture = cv2.VideoCapture(0)
     while videoCapture.isOpened():
-        _, frame = videoCapture.read()
-        _img = recognize(frame, clf, faceCascade)
-        stream = cv2.imencode('.jpeg', _img)[1].tobytes()
+        _, image = videoCapture.read()
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        features = faceCascade.detectMultiScale(gray_image, 1.3, 5)
+        for top, right, bottom, left in features:
+            cv2.rectangle(image, (top, right), (top + bottom,
+                          right + left), (26, 174, 10), 2)
+            cv2.rectangle(image, (top - 1, right + left), (top + 1 +
+                          bottom, right + left + 40), (26, 174, 10), cv2.FILLED)
+            id, pred = clf.predict(
+                gray_image[right:right + left, top:top + bottom])
+            confidence = int(100 * (1 - pred / 300))
+            cursor.execute(
+                "SELECT p.p_name FROM img_dataset AS i LEFT JOIN person_data AS p ON i.img_person = p.p_id WHERE i.img_id = " + str(id))
+            row = cursor.fetchone()
+            uname = row[0]
+            if confidence > 70:
+                label = uname + " (" + str(confidence) + "%)"
+                cv2.putText(image, label, (top + 4, right + left + 28),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(image, "Unknown", (top + 4, right + left + 14),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        stream = cv2.imencode('.jpeg', image)[1].tobytes()
         yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + stream + b'\r\n'
         if cv2.waitKey(1) == 27:
             videoCapture.release()
@@ -118,64 +159,104 @@ def get_face_recognition():
     else:
         print("camera not streaming.")
 
+
 def get_face_check_in():
-    def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, _, clf):
-        global pause_count
-        global scanned
-
-        pause_count += 1
-
-        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        features = classifier.detectMultiScale(gray_image, scaleFactor, minNeighbors)
-        
-        for top, right, bottom, left in features:
-            cv2.rectangle(img, (top, right), (top + bottom, right + left), color, 2)
-            id, pred = clf.predict(gray_image[right:right + left, top:top + bottom])
-            cursor.execute("SELECT p.p_id, p.p_name FROM img_dataset AS i LEFT JOIN person_data AS p ON i.img_person = p.p_id WHERE i.img_id = " + str(id))
-            row = cursor.fetchone()
-            uid = row[0]
-            uname = row[1]
-            confidence = int(100 * (1 - pred / 300))
-            if confidence > 70 and not scanned:
-                global count
-                count += 1
-                n = (100 / 15) * count
-                w_filled = (count / 15) * bottom
-                cv2.rectangle(img, (top - 1, right + left), (top + 1 + int(w_filled), right + left + 20), color, cv2.FILLED)
-                cv2.putText(img, str(int(n))+' %', (top + 4, right + left + 14), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255), 1, cv2.LINE_AA)
-
-                if int(count) == 15:
-                    count = 0
-                    cursor.execute("INSERT INTO check_in(p_id) VALUES({})".format(uid))
-                    conn.commit()
-                    scanned = True
-                    pause_count = 0
-            else:
-                if not scanned:
-                    cv2.rectangle(img, (top - 1, right + left), (top + 1 + bottom, right + left + 20), color, cv2.FILLED)
-                    cv2.putText(img, "Unknown", (top + 4, right + left + 14), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255), 1, cv2.LINE_AA)
-                else:
-                    cv2.rectangle(img, (top - 1, right + left), (top + 1 + bottom, right + left + 20), color, cv2.FILLED)
-                    cv2.putText(img, str(uname) + ": Checked in", (top + 4, right + left + 14), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255), 1, cv2.LINE_AA)
-                    
-                    deylay = 100
-                    if pause_count > deylay: # delay
-                        scanned = False
-        return True
-
-    def recognize(img, clf, faceCascade):
-        draw_boundary(img, faceCascade, 1.1, 10, (26, 174, 10), "Face", clf)
-        return img
-
-    faceCascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
+    faceCascade = cv2.CascadeClassifier(
+        "resources/haarcascade_frontalface_default.xml")
     clf = cv2.face.LBPHFaceRecognizer_create()
     clf.read("resources/classifier.xml")
 
     videoCapture = cv2.VideoCapture(0)
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDrawing = mp.solutions.drawing_utils
+    mpDrawingStyles = mp.solutions.drawing_styles
+
+    i = 0
     while videoCapture.isOpened():
-        _, frame = videoCapture.read()
-        _img = recognize(frame, clf, faceCascade)
-        stream = cv2.imencode('.jpeg', _img)[1].tobytes()
+        _, image = videoCapture.read()
+        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(imageRGB)
+
+        fingerCount = 0
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:  # working with each hand
+
+                handIndex = results.multi_hand_landmarks.index(handLms)
+                handLabel = results.multi_handedness[handIndex].classification[0].label
+                # print(handLabel)
+
+                handLandmarks = []
+                for landmarks in handLms.landmark:
+                    handLandmarks.append([landmarks.x, landmarks.y])
+
+                if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+                    fingerCount = fingerCount+1
+                elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
+                    fingerCount = fingerCount+1
+
+                if handLandmarks[8][1] < handLandmarks[6][1]:  # นิ้วชี้
+                    fingerCount = fingerCount+1
+                if handLandmarks[12][1] < handLandmarks[10][1]:  # นิ้วกลาง
+                    fingerCount = fingerCount+1
+                if handLandmarks[16][1] < handLandmarks[14][1]:  # นิ้วนาง
+                    fingerCount = fingerCount+1
+                if handLandmarks[20][1] < handLandmarks[18][1]:  # นิ้วก้อย
+                    fingerCount = fingerCount+1
+
+                mpDrawing.draw_landmarks(
+                    image,
+                    handLms,
+                    mpHands.HAND_CONNECTIONS,
+                    mpDrawingStyles.get_default_hand_landmarks_style(),
+                    mpDrawingStyles.get_default_hand_connections_style()
+                )
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        features = faceCascade.detectMultiScale(gray_image, 1.3, 5)
+        for top, right, bottom, left in features:
+            cv2.rectangle(image, (top, right), (top + bottom,
+                          right + left), (26, 174, 10), 2)
+            cv2.rectangle(image, (top - 1, right + left), (top + 1 +
+                          bottom, right + left + 40), (26, 174, 10), cv2.FILLED)
+            id, pred = clf.predict(
+                gray_image[right:right + left, top:top + bottom])
+            confidence = int(100 * (1 - pred / 300))
+            cursor.execute(
+                "SELECT p.p_id, p.p_name FROM img_dataset AS i LEFT JOIN person_data AS p ON i.img_person = p.p_id WHERE i.img_id = " + str(id))
+            row = cursor.fetchone()
+            uid = row[0]
+            uname = row[1]
+            if confidence > 70:
+                label = uname + " (" + str(confidence) + "%)"
+                cv2.putText(image, label, (top + 4, right + left + 28),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+                if fingerCount == 5:
+                    status = 'Checking in...'
+                    i += 1
+                    loading = (i / 15) * bottom
+                    cv2.rectangle(image, (top, right + left + 40), (top + 1 +
+                                  int(loading), right + left + 50), (0, 255, 255), cv2.FILLED)
+                    if i == 15:
+                        i = 0
+                        cursor.execute(
+                            "INSERT INTO check_in(p_id) VALUES({})".format(uid))
+                        conn.commit()
+                        status = 'Success'
+                        playsound(
+                            'static/sounds/success-sound-effect.mp3', True)
+
+                    cv2.putText(image, status, (top, right - 5),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+                else:
+                    i = 0
+            else:
+                cv2.putText(image, "Unknown", (top + 4, right + left + 14),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        stream = cv2.imencode('.jpeg', image)[1].tobytes()
         yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + stream + b'\r\n'
         if cv2.waitKey(1) == 27:
             videoCapture.release()
@@ -188,7 +269,8 @@ def get_face_check_in():
 ###################### route zone ######################
 @app.route('/')
 def index():
-    cursor.execute("SELECT p_id, p_name, p_created FROM person_data WHERE p_status = 1")
+    cursor.execute(
+        "SELECT p_id, p_name, p_created FROM person_data WHERE p_status = 1")
     data = cursor.fetchall()
     return render_template('index.html', data=data)
 
@@ -210,7 +292,8 @@ def add_person():
 def add_person_submit():
     pid = request.form.get('p_id')
     pname = request.form.get('p_name')
-    cursor.execute("""INSERT INTO `person_data`(`p_id`, `p_name`) VALUES('{}', '{}')""".format(pid, pname))
+    cursor.execute(
+        """INSERT INTO `person_data`(`p_id`, `p_name`) VALUES('{}', '{}')""".format(pid, pname))
     conn.commit()
     return redirect(url_for('create_dataset', pid=pid))
 
@@ -262,6 +345,7 @@ def check_in_feed():
 @app.route('/check_in')
 def check_in():
     return render_template('check_in.html')
+
 
 ###################### start local server ######################
 if __name__ == '__main__':
